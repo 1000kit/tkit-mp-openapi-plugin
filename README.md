03b9bb4f6a578701504b17bf6f9dc1af7e4acf8d
Andrej Petras <andrej.petras@capgemini.com>
2019-11-08 15:33:28 +0100

    Create micro-profile openAPI maven plugin

--- README.md
+++ README.md
@@ -1,3 +1,62 @@
 # tkit-mp-openapi-plugin
 
 1000kit microprofile openapi plugin
+
+[![License](https://img.shields.io/badge/license-Apache--2.0-green?style=for-the-badge&logo=apache)](https://www.apache.org/licenses/LICENSE-2.0)
+[![Maven Central](https://img.shields.io/maven-central/v/org.1000kit.maven/tkit-mp-openapi-plugin?logo=java&style=for-the-badge)](https://maven-badges.herokuapp.com/maven-central/org.1000kit.maven/tkit-mp-openapi-plugin)
+
+## Goal: generate
+
+```xml
+<plugin>
+    <groupId>org.1000kit.mp</groupId>
+    <artifactId>tkit-mp-openapi-plugin</artifactId>
+    <version>0.1.0-SNAPSHOT</version>
+    <executions>
+        <execution>
+            <id>generate</id>
+            <goals>
+                <goal>generate</goal>
+            </goals>
+            <configuration>
+                <verbose>true</verbose>
+                <classesDir>${project.build.outputDirectory}</classesDir>
+                <configFile>src/main/my.properties</configFile>
+                <configFileOrdinal>200</configFileOrdinal>
+                <properties>
+                    <mp.openapi.scan.exclude.packages>org.tkit.parameters.rs.external.v2</mp.openapi.scan.exclude.packages>
+                </properties>
+                <propertiesOrdinal>201</propertiesOrdinal>
+                <format>YAML</format>
+                <outputFile>${project.build.directory}/openapi.yaml</outputFile>
+            </configuration>
+        </execution>
+    </executions>
+</plugin>
+```
+
+#### Parameters
+
+|  Name | Default  | Values | Description  |
+|---|---|---|---|
+| verbose  | false | | The verbose flag  |
+| classDir | ${project.build.outputDirectory} | | Directory of the classes |
+| configFile | | | The micro-profile configuration property file |
+| configFileOrdinal | 200 | | The micro-profile configuration property file ordinal number |
+| properties | | | The micro-profile configuration properties in the plugin configuration |
+| propertiesOrdinal | 201 | | The micro-profile configuration properties ordinal number |
+| format | YAML | YAML, JSON | The output format |
+| outputFile | ${project.build.directory}/openapi.yaml | | The output openAPI file |
+
+## Release
+
+### Create a release
+
+```bash
+mvn semver-release:release-create
+```
+
+### Create a patch branch
+```bash
+mvn semver-release:patch-create -DpatchVersion=x.x.0
+```
---
+++ src/main/java/org/tkit/maven/mp/openapi/GenerateOpenApiMojo.java
@@ -0,0 +1,213 @@
+/*
+ * Copyright 2019 1000kit.org.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tkit.maven.mp.openapi;
+
+import io.smallrye.config.PropertiesConfigSource;
+import io.smallrye.config.SmallRyeConfig;
+import io.smallrye.openapi.api.OpenApiConfig;
+import io.smallrye.openapi.api.OpenApiConfigImpl;
+import io.smallrye.openapi.runtime.io.OpenApiSerializer;
+import io.smallrye.openapi.runtime.scanner.OpenApiAnnotationScanner;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecution;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.shared.utils.io.DirectoryScanner;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.spi.ConfigSource;
+import org.eclipse.microprofile.openapi.models.OpenAPI;
+import org.jboss.jandex.ClassInfo;
+import org.jboss.jandex.Indexer;
+import org.reflections.Reflections;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.*;
+
+/**
+ * Generate the openAPI file.
+ */
+@Mojo(name = "generate",
+        defaultPhase = LifecyclePhase.PROCESS_CLASSES,
+        requiresProject = true,
+        aggregator = false,
+        threadSafe = true)
+public class GenerateOpenApiMojo extends AbstractMojo {
+
+    /**
+     * The classes directory.
+     */
+    @Parameter(defaultValue = "${project.build.outputDirectory}")
+    private File classesDir;
+
+    /**
+     * The configuration file.
+     */
+    @Parameter(name = "configFile")
+    private File configFile;
+
+    /**
+     * The configuration file ordinal number.
+     */
+    @Parameter(name = "configFileOrdinal", defaultValue = "200", required = true)
+    private String configFileOrdinal;
+
+    /**
+     * The output file.
+     */
+    @Parameter(name = "outputFile", defaultValue = "${project.build.directory}/openapi.yaml", required = true)
+    private File outputFile;
+
+    /**
+     * The format: YAML, JSON
+     */
+    @Parameter(name = "format", defaultValue = "YAML", required = true)
+    private String format;
+
+    /**
+     * The verbose flag.
+     */
+    @Parameter(defaultValue = "false")
+    private boolean verbose;
+
+    /**
+     * The micro-profile properties defined in the plugin configuration.
+     */
+    @Parameter(name = "properties")
+    private Properties properties;
+
+    /**
+     * The micro-profile properties defined in the plugin configuration ordinal number.
+     */
+    @Parameter(name = "propertiesOrdinal", defaultValue = "201", required = true)
+    private String propertiesOrdinal;
+
+    /**
+     * The mojo execution
+     */
+    @Parameter(defaultValue = "${mojoExecution}", readonly = true)
+    private MojoExecution mojoExecution;
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try {
+            Indexer indexer = createIndexer();
+            if (indexer == null) {
+                return;
+            }
+
+            OpenApiAnnotationScanner scanner = new OpenApiAnnotationScanner(createConfig(), indexer.complete());
+            OpenAPI result = scanner.scan();
+
+            OpenApiSerializer.Format f = OpenApiSerializer.Format.YAML;
+            if (OpenApiSerializer.Format.JSON.name().equals(format)) {
+                f = OpenApiSerializer.Format.JSON;
+            }
+
+            String output = OpenApiSerializer.serialize(result, f);
+            if (verbose) {
+                getLog().info("OpenApi:\n" + output);
+            }
+            getLog().info("OpenAPI file in format " + format + " created: " + outputFile);
+            Files.write(outputFile.toPath(), output.getBytes(StandardCharsets.UTF_8));
+
+        } catch (Exception ex) {
+            throw new MojoExecutionException("Error execute the plugin ", ex);
+        }
+    }
+
+    /**
+     * Creates the micro-profile configuration for the openAPI
+     *
+     * @return the openAPI configuration.
+     * @throws MojoExecutionException if the method fails.
+     */
+    private OpenApiConfig createConfig() throws MojoExecutionException {
+        Config config = ConfigProvider.getConfig();
+        SmallRyeConfig sc = (SmallRyeConfig) config;
+
+        // config file
+        if (configFile != null && configFile.exists() && configFile.isFile()) {
+            Properties prop = new Properties();
+            try (InputStream input = new FileInputStream(configFile)) {
+                prop.load(input);
+            } catch (Exception e) {
+                throw new MojoExecutionException("Error loading the properties " + configFile, e);
+            }
+            prop.setProperty(ConfigSource.CONFIG_ORDINAL, configFileOrdinal);
+            sc.addConfigSource(new PropertiesConfigSource(prop, configFile.getName()));
+            getLog().info("Add the configuration source " + configFile);
+        }
+
+        // properties in the plugin configuration
+        if (properties != null && !properties.isEmpty()) {
+            properties.setProperty(ConfigSource.CONFIG_ORDINAL, propertiesOrdinal);
+            String id = mojoExecution.getMojoDescriptor().getId() + " (" + mojoExecution.getExecutionId() + ")";
+            sc.addConfigSource(new PropertiesConfigSource(properties, id));
+            getLog().info("Add the configuration source from maven plugin: " + id);
+        }
+
+        return new OpenApiConfigImpl(config);
+    }
+
+    /**
+     * Creates the indexer
+     *
+     * @return the indexer
+     * @throws MojoExecutionException if the method fails.
+     */
+    private Indexer createIndexer() throws MojoExecutionException {
+
+        if (!classesDir.exists()) {
+            getLog().info("Directory does not exist! Directory: " + classesDir);
+            return null;
+        }
+
+        final DirectoryScanner scanner = new DirectoryScanner();
+        scanner.setBasedir(classesDir);
+        scanner.setIncludes("**/*.class");
+        scanner.scan();
+        final String[] files = scanner.getIncludedFiles();
+
+        final Indexer indexer = new Indexer();
+        for (final String file : files) {
+            if (file.endsWith(".class")) {
+                File tmp = new File(classesDir, file);
+                if (verbose) {
+                    getLog().info("File: " + tmp);
+                }
+                try (FileInputStream fis = new FileInputStream(tmp)) {
+                    final ClassInfo info = indexer.index(fis);
+                    if (verbose && info != null) {
+                        getLog().info("Indexed " + info.name() + " (" + info.annotations().size() + " annotations)");
+                    }
+                } catch (final Exception e) {
+                    throw new MojoExecutionException(e.getMessage(), e);
+                }
+
+            }
+        }
+        return indexer;
+    }
+}
